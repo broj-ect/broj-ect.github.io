c53e5ef5d99502ed6e74a30ed64ba64b205052c1
Antti V <johhni.games@gmail.com>
2023-12-18 15:25:57 +0200

    build front for locker test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 export function App() {
+  setInterval(updatetime, 10000);
   const now = new Date().toLocaleTimeString();
   const [time, setTime] = useState(now);
-  setInterval(updatetime, 5000);
 
   function updatetime() {
     const newtime = new Date().toLocaleTimeString();
@@ -12,7 +12,7 @@ export function App() {
 
   const [photos, setPhotos] = useState([]);
   useEffect(() => {
-    fetch("https://api.broject.cc/v1/locker")
+    fetch("https://api.broject.cc/v1/locker",{})
       .then((res) => {
         return res.json();
       })
@@ -23,8 +23,7 @@ export function App() {
   }, [time]);
 
   return (
-    <>
-      <div className="wrapper" style={{ marginTop: 150 }}>
+    <> <div className="wrapper" style={{ marginTop: 150 }}>
         {photos.map((photo, index) => {
           if (photo.Locker.Size === "screen")
             return (
@@ -60,7 +59,7 @@ export function App() {
                   color: "white",
                 }}
               >
-                {photo.Char} {photo.Locker.Num}
+                <p>{photo.Char} {photo.Locker.Num}</p>
               </div>
             );
           }
@@ -81,7 +80,70 @@ export function App() {
                 border: "solid",
               }}
             >
-              {photo.Char} {photo.Locker.Num}
+              <p>{photo.Char} {photo.Locker.Num}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div style={{padding:150}}></div>
+      <div className="wrapper flipped" style={{ marginTop: 150 }}>
+        {photos.map((photo, index) => {
+          if (photo.Locker.Size === "screen")
+            return (
+              <div
+                key={photo.Locker.Num}
+                className={photo.Locker.Size}
+                style={{
+                  border: "solid black",
+                  fontWeight: "bold",
+                }}
+              ></div>
+            );
+          if (photo.Locker.State) {
+            return (
+              <div
+                key={photo.Locker.Num}
+                className={photo.Locker.Size}
+                onClick={() => {
+                  fetch(
+                    "https://api.broject.cc/v1/locker/" + photo.Locker.Num,
+                    {
+                      method: "PATCH",
+                    },
+                  );
+                  /*setTimeout(() => {
+                    window.location.reload(false);
+                  }, 100);*/
+                }}
+                style={{
+                  border: "solid black",
+                  fontWeight: "bold",
+                  background: "red",
+                  color: "white",
+                }}
+              >
+                <p>{photo.Char} {photo.Locker.Num}</p>
+              </div>
+            );
+          }
+          return (
+            <div
+              key={photo.Locker.Num}
+              className={photo.Locker.Size}
+              onClick={() => {
+                fetch("https://api.broject.cc/v1/locker/" + photo.Locker.Num, {
+                  method: "PATCH",
+                });
+                /*setTimeout(() => {
+                  window.location.reload(false);
+                }, 100);*/
+              }}
+              style={{
+                fontWeight: "bold",
+                border: "solid",
+              }}
+            >
+              <p>{photo.Char} {photo.Locker.Num}</p>
             </div>
           );
         })}
